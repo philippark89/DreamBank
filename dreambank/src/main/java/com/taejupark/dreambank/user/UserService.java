404275882ba01c8b32b678dc2c1b1b08c9a53875
Taeju Park <philippark89@gmail.com>
2023-09-20 15:19:35 -0500

    testing out

--- dreambank/src/main/java/com/taejupark/dreambank/user/UserService.java
+++ dreambank/src/main/java/com/taejupark/dreambank/user/UserService.java
@@ -4,7 +4,7 @@ import com.taejupark.dreambank.security.UserRegistrationDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
-
+    User findById(Long id);
     User findByEmail(String email);
     User save(UserRegistrationDto registration);
 }
